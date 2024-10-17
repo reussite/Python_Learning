b1ea4cab3fa556b9a4a768cf105986c638c4227b
reussite <pascal.dlm@yahoo.com>
2024-10-17 01:13:24 +0100

    add types of variables in python

---
+++ variables/variable.py
@@ -0,0 +1,29 @@
+# Les types de données et les variables en Python 
+# Il existe plusieurs types de données en python : 
+
+"""
+  Les types de données et les variables en Python.
+  Il existe plusieurs types de données en python : 
+  1. Integer (int) : Nombres entiers positifs ou négatifs.
+  2. Float (float) : Nombres décimaux.
+  3. String (str) : Chaînes de caractères.
+  4. Boolean (bool) : Vrai ou faux.
+  5. List (list) : Une collection ordonnée de données.
+  6. Tuple (tuple) : Une collection ordonnée de données immuable.
+  7. Dictionary (dict) : Un ensemble de paires clé-valeur.
+  8. Set (set) : Une collection non ordonnée sans doublon.
+  9. Fonction (function) : Un ensemble d'instructions (n'est pas un type de donnée en soi).
+  10. Module (module) : Fichier contenant du code Python réutilisable (n'est pas un type de donnée en soi).
+  11. Classe (class) : Modèle pour créer des objets avec des propriétés et des méthodes (n'est pas un type de donnée en soi).
+
+"""
+
+
+# Déclaration et affectation de variables
+
+nom =  "John"  
+age = 30
+citation = " "
+
+
+
